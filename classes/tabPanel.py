38222867d6bdc4e0d7975b84d0297d7438d4a0c6
Oleksandrigo <oleksandrigo@gmail.com>
2024-12-04 19:24:02 +0200

    Enhance TabPanel functionality: Added ScrollableButton class for improved scrolling behavior in tabbed interface. Updated TabPanel to disable vertical and horizontal scroll bars, and set a transparent background for the scroll widget. Enhanced tab creation with mouse tracking and event linking for better user experience.

--- classes/tabPanel.py
+++ classes/tabPanel.py
@@ -1,6 +1,17 @@
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtCore import Qt
 
 from styles.material import MaterialColor, MaterialIconPushButton, MaterialScrollArea
+
+class ScrollableButton(MaterialIconPushButton):
+    def __init__(self, scroll_area, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.scroll_area: MaterialScrollArea = scroll_area
+
+    def wheelEvent(self, event) -> None:
+        self.scroll_area.horizontalScrollBar().setValue(
+            self.scroll_area.horizontalScrollBar().value() - event.angleDelta().y()
+        )
 
 class TabPanel(QWidget):
     def __init__(self) -> None:
@@ -24,10 +35,18 @@ class TabPanel(QWidget):
         self.bg.setLayout(self.vbox)
 
         self.scroll_tabs = MaterialScrollArea()
+        self.scroll_tabs.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.scroll_tabs.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.scroll_tabs.setWidgetResizable(True)
         self.vbox.addWidget(self.scroll_tabs, stretch=1)
 
         self.scroll_widget = QWidget()
+        self.scroll_widget.setObjectName("transparent-bg")
+        self.scroll_widget.setStyleSheet(f"""
+            #transparent-bg {{
+                background-color: {MaterialColor.transparent_color};
+            }}
+        """)
         self.scroll_tabs.setWidget(self.scroll_widget)
 
         self.tabs_hbox = QHBoxLayout()
@@ -35,7 +54,11 @@ class TabPanel(QWidget):
         self.scroll_widget.setLayout(self.tabs_hbox)
 
         for i in range(100):
-            tab = MaterialIconPushButton(text="123", height=40, width=140)
+            tab = ScrollableButton(scroll_area=self.scroll_tabs, text="123", height=40, width=140)
+            tab.setMouseTracking(True)  # Enable mouse tracking for the button
+            tab.enterEvent = self.scroll_tabs.enterEvent  # Link enter event to scroll
+            tab.leaveEvent = self.scroll_tabs.leaveEvent  # Link leave event to scroll
+
             self.tabs_hbox.addWidget(tab)
 
         self.data_from_tabs = QVBoxLayout()
